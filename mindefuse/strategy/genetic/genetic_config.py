a4a49886339c271243ff84d1f4b9dfbf62127191
Kronostheus <nuno.amaro@tecnico.ulisboa.pt>
2019-05-16 16:50:10 +0100

    Genetic Algorithm Implementation (#2)
    Add Genetic algorithm
    
    Some unit tests still need to be added

---
+++ mindefuse/strategy/genetic/genetic_config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.7
+
+
+class GeneticConfig:
+    """
+    Configuration of Genetic Algorithm
+    """
+
+    CROSSOVER_PROB = 0.5
+    MUTATION_PROB = 0.03
+    PERMUTATION_PROB = 0.03
+    INVERSION_PROB = 0.02
+
+    MAX_POPULATION = 60
+    MAX_GENERATIONS = 120
